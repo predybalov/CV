bcf52a55aced927ce02ea9035dde128e4e5c05ad
predybalov <79003995+predybalov@users.noreply.github.com>
2021-12-21 10:45:27 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.70.0"
+    }
+  }
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-06bfd6343550d4a29"
+  instance_type = "t3.micro"
+}
