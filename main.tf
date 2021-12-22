a698f26e7a1a8d68f0a157a6da5beb913c75c071
predybalov <79003995+predybalov@users.noreply.github.com>
2021-12-22 16:29:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,6 +82,26 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
+resource "aws_route53_record" "main" {
+  zone_id = "Z040603910U6C3CYA7VRW"
+  name    = "predybalov.link"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_eip.CV.public_ip]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z040603910U6C3CYA7VRW"
+  name    = "www.predybalov.link"
+  type    = "A"
+
+  alias {
+    name                   = aws_route53_record.main.name
+    zone_id                = "Z040603910U6C3CYA7VRW"
+    evaluate_target_health = true
+  }
+}
+
 output "instance_ip_addr" {
   value = aws_eip.CV.public_ip
 }
