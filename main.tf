0e938303f93d433db38be78d8eb410905ebb044a
predybalov <79003995+predybalov@users.noreply.github.com>
2021-12-24 14:31:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,9 +64,12 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOT
                  #!/bin/sh
-                 aws configure set region eu-north-1
-                 aws configure set aws_access_key_id ${var.aws_key_id}
-                 aws configure set aws_secret_access_key "${var.aws_secret_key}"
+                 mkdir /home/ec2-user/.aws
+                 echo "[default]" >> /home/ec2-user/.aws/config
+                 echo "region = eu-north-1" >> /home/ec2-user/.aws/config
+                # aws configure set region eu-north-1
+                # aws configure set aws_access_key_id ${var.aws_key_id}
+                # aws configure set aws_secret_access_key "${var.aws_secret_key}"
                 # mkdir /home/ec2-user/ssl
                 # docker run -d -p 80:80 -p 443:443 ${var.docker_image}
                  EOT
