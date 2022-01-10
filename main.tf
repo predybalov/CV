9b044b858b603ed182321e21a46c581f112962d5
predybalov <gelios@gelios.com>
2022-01-10 12:57:05 +0300

    Move terraform variable to separate file

--- main.tf
+++ main.tf
@@ -16,44 +16,8 @@ terraform {
   }
 }
   
-variable "docker_image" {
-  type = string
-  description = "Docker image name"
-  default = "docker:image"
-}
-
-variable "aws_key_id" {
-  type = string
-  description = "AWS Key ID"
-  default = "ki"
-}
-
-variable "aws_secret_key" {
-  type = string
-  description = "AWS Secret Key"
-  default = "sk"
-}
-
-variable "aws_region" {
-  type = string
-  description = "AWS Default Region"
-  default = "dr"
-}
-
-variable "aws_format" {
-  type = string
-  description = "AWS Format"
-  default = "json"
-}
-
-variable "aws_cert_bucket" {
-  type = string
-  description = "Certificate storage"
-  default = "cb"
-}
-
 provider "aws" {
-  region                  = "eu-north-1"
+  region = var.aws_region
 }
 
 # ssh-key for debug
