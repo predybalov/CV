947767901f873aafea84ae408a1484de3770daef
predybalov <gelios@gelios.com>
2022-01-20 13:01:51 +0300

    Revert "changes"
    This reverts commit 9a2cd62829f3170ba4e8a3a6b7abb7fc5ab0ac9c.

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "CV" {
-  instance = aws_instance.CV_instance.id
+  instance = aws_instance.web.id
   vpc      = true
 
   tags = {
@@ -7,8 +7,8 @@ resource "aws_eip" "CV" {
   }
 }
 
-resource "aws_security_group" "CV_sg" {
-  name = "CV_sg"
+resource "aws_security_group" "web-sg" {
+  name = "sgCV"
   ingress {
     from_port   = 22
     to_port     = 22
